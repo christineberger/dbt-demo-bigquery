43dca9dafd8fccea8e4292c3b87c7ef2bf079666
Christine Berger <christine@dbtlabs.com>
2022-04-05 18:46:37 +0000

    Cleanup for BQ

--- models/demo_examples/get_area_of_circle.sql
+++ models/demo_examples/get_area_of_circle.sql
@@ -1,2 +1,2 @@
-
+{{ config(enabled=false) }}
 SELECT area_of_circle(1.0) AS area
--- models/marts/aggregates/agg_ship_modes_hardcoded_pivot.sql
+++ models/marts/aggregates/agg_ship_modes_hardcoded_pivot.sql
@@ -1,26 +1,39 @@
 /* Create a pivot table with hard-coded columns based on a query of the ship modes that are in the system */
 
-with merged as (
+with 
+
+merged as (
+ 
     select
-        date_part('year', order_date) as order_year,
+      
+        extract(year from order_date) as order_year,
         ship_mode,
         gross_item_sales_amount
+    
     from {{ ref('fct_order_items') }}
+
+),
+
+final as (
+    
+    select * from merged
+    -- have to manually map strings in the pivot operation
+    pivot(
+        sum(gross_item_sales_amount) 
+        for ship_mode
+        in (
+            "AIR",
+            "REG_AIR",
+            "FOB",
+            "RAIL",
+            "MAIL",
+            "SHIP",
+            "TRUC"
+        )
+    ) as p 
+
+    order by order_year
+
 )
 
-select
-    * 
-from
-    merged
-    -- have to manually map strings in the pivot operation
-    pivot(sum(gross_item_sales_amount) for ship_mode in (
-        'AIR',
-        'REG AIR',
-        'FOB',
-        'RAIL',
-        'MAIL',
-        'SHIP',
-        'TRUCK'
-    )) as p 
-
-order by order_year
+select * from final
